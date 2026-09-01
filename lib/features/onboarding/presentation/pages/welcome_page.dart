cb0611707f8f9a3d5ce6ebe1697e54408b0ad4ad
saif <SaifQaderFawzi1235789@gmail.com>
2026-09-02 01:09:40 +0300

    Add an image over the blurred element

--- lib/features/onboarding/presentation/pages/welcome_page.dart
+++ lib/features/onboarding/presentation/pages/welcome_page.dart
@@ -10,10 +10,22 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF051232),
-      body: Container(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GlowCircle(),
+            SizedBox(height: 72),
+            Stack(
+              children: [
+                const GlowCircle(),
+                Image.asset(
+                  "assets/images/welcome_page_image.jpg",
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
           ],
         ),
       ),
